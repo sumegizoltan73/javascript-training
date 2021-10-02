2ff6708cfc88547068497136ecc6ecfededd8caf
Sümegi Zoltán <sumegizoltan73@gmail.com>
2021-10-02 17:30:30 +0200

    fixed settimeout

--- async_js/promise2.js
+++ async_js/promise2.js
@@ -26,14 +26,14 @@ const yt = new Promise(resolve => {
     setTimeout(() => {
         console.log('gotting stuff from yt');
         resolve({videos: [1,2,3,4,5]});
-    }), 2000;
+    }, 2000);
 });
 
 const fb = new Promise(resolve => {
     setTimeout(() => {
         console.log('stuff from fb');
         resolve({user: 'Name'});
-    }), 2000;
+    }, 2000);
 });
 
 Promise.all([yt, fb]).then(result => console.log(result));
